8e7e8cd776a83a52e092ad51dbda11a007fe5c57
rmsckd1640 <rmsckd1640@gmail.com>
2026-09-10 14:54:19 +0900

    first commit

---
+++ src/main/java/kr/co/cking/CkingBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/cking/CkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.cking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
